97f473d544fed84e28c8f256b1231e8f4c227292
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-06 21:41:00 +0100

    refine dreamweaver

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -385,8 +385,7 @@ PROFILES = {
     "features": [
         {"type": "var", "key": ["freq_percept_intense"],     "norm": norm_1_6, "norm_kwargs": {}, "target": 0.70, "weight": 1},
         {"type": "var", "key": ["freq_percept_narrative"],   "norm": norm_1_6, "norm_kwargs": {}, "target": 0.80, "weight": 1},
-        {"type": "var", "key": ["freq_percept_imposed"],     "norm": norm_1_6, "norm_kwargs": {}, "target": 0.70, "weight": 1},
-        {"type": "var", "key": ["freq_absorbed"],            "norm": norm_1_6, "norm_kwargs": {}, "target": 0.80, "weight": 1.0},
+        {"type": "var", "key": ["freq_absorbed"],            "norm": norm_1_6, "norm_kwargs": {}, "target": 0.70, "weight": 1.0},
         {"type": "var", "key": ["degreequest_vividness"] ,   "norm": norm_1_6, "norm_kwargs": {}, "target": 0.70, "weight": 0.8},  
         {"type": "var", "key": ["degreequest_bizarreness"] , "norm": norm_1_6, "norm_kwargs": {}, "target": 0.70, "weight": 0.8},  
     
