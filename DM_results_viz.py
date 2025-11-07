fffb81cbe4f17214f6e48582c382fb3ed82a2693
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-07 18:05:12 +0100

    button name

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -1271,7 +1271,7 @@ components.html(
 </head>
 <body>
   <div style="max-width:820px; margin:12px auto 0; text-align:center;">
-    <button id="dmshot" class="bar">📸 Download this section (PNG)</button>
+    <button id="dmshot" class="bar">📸 Download your Drifting Mind (PNG)</button>
   </div>
 
   <!-- Hidden mirror of your section -->
