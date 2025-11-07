9681843ffccd806f6a10baa05f1f830576c835ba
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-07 15:32:22 +0100

    adjust subtitle

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -1714,7 +1714,7 @@ with exp_right:
     ax.set_facecolor("none")
     ax.axis("off")
     
-    # ax.set_title("Dynamics of your experience", fontsize=10, pad=6, color="#222222")
+    ax.set_title("Dynamics of your experience", fontsize=16, pad=2, color="#222222")
 
 
     # Geometry
