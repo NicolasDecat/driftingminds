2e7197f857d817d23d3152f10c94a1d8391a3df4
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-09 18:55:58 +0100

    qr

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -56,7 +56,7 @@ st.markdown(
     f"""
     <div style="
         position: absolute;
-        top: 5px;          /* now truly the top of the content area */
+        top: 10px;          /* now truly the top of the content area */
         right: 0px;        /* hugs the content's right edge */
         text-align: center;
         font-size: 0.9rem;
