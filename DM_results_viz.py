ae09f7d35d0c25e89d35105e2f3f8495279708eb
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-10 15:35:29 +0100

    fix placement

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -2326,10 +2326,10 @@ st.markdown(
         display: flex;
         justify-content: center;
         align-items: center;
-        margin: 40px 0;
+        margin: 80px 0;
     ">
         <img src="data:image/png;base64,{all_dms_b64}" 
-             style="max-width: 90%; height: auto; border-radius: 8px;">
+             style="max-width: 70%; height: auto; border-radius: 8px;">
     </div>
     """,
     unsafe_allow_html=True
