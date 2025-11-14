a3c640b6d7ee0c5ad78afaad65adf9ac026aefc1
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-14 12:25:13 +0100

    fix ranking

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -3338,7 +3338,7 @@ try:
                     im = OffsetImage(img, zoom=0.08)  # tweak zoom if icons too big/small
 
                     # Place icon a bit above the bar
-                    icon_y = p + (y_max - y_min) * 0.04
+                    icon_y = p + (y_max - y_min) * 0.02
                     ab = AnnotationBbox(im, (xi, icon_y), frameon=False)
                     ax.add_artist(ab)
                 except Exception:
