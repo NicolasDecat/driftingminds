9e42d32e7220fce2aed13d89a832b7d7d50aa1a6
NicolasDecat <nicolasdecat59@gmail.com>
2025-11-19 16:37:28 +0100

    translation fix

--- DM_results_viz.py
+++ DM_results_viz.py
@@ -975,10 +975,10 @@ TEXT = {
     # -----------------------
     # RADAR LABELS
     # -----------------------
-    "vivid": {"en": "vivid", "fr": "vive", "es": "vívido"},
-    "immersive": {"en": "immersive", "fr": "immersive", "es": "inmersivo"},
+    "vivid": {"en": "vivid", "fr": "vif", "es": "vívido"},
+    "immersive": {"en": "immersive", "fr": "immersif", "es": "inmersivo"},
     "bizarre": {"en": "bizarre", "fr": "bizarre", "es": "extraño"},
-    "spontaneous": {"en": "spontaneous", "fr": "spontanée", "es": "espontáneo"},
+    "spontaneous": {"en": "spontaneous", "fr": "spontané", "es": "espontáneo"},
     "fleeting": {"en": "fleeting", "fr": "fugace", "es": "fugaz"},
     "positive\nemotions": {
         "en": "positive\nemotions",
